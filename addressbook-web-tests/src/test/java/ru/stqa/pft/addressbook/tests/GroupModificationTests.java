38f62c1919a56967b9a45b5972966644a4e8861f
aenelle <aenelle@gmail.com>
2021-02-09 02:25:27 +0300

    Делаем новый тест без использования рекордера (Лекция 2.9. )

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupDate;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testCroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupDate("test1", "test2", "test2"));
+        app.getGroupHelper().submitGroupModification();
+
+    }
+
+}

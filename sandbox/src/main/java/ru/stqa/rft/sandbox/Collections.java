9dfc6b6e4e105c11910e997c9372d6130bd10e58
aenelle <aenelle@gmail.com>
2021-02-23 22:48:38 +0300

    Промежуточное сохранение (Лекция 4.2)

---
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/Collections.java
@@ -0,0 +1,17 @@
+package ru.stqa.rft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+  public static void main(String[] args) {
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+    for (String l : langs) {
+      System.out.println("Я хочу выучить " + l);
+    }
+  }
+}
+

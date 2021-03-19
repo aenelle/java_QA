591c26301b2c6b137ed5784c82fc88c82c245d4f
aenelle <aenelle@gmail.com>
2021-03-19 14:19:43 +0300

    Лекция 6.3. Анализ опций командой строки при запуске генератора данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
 import java.io.File;
@@ -10,16 +13,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
+@Parameter(names = "-c", description = "Group count")
+  public int count;
+@Parameter(names = "-f", description = "Target file")
+public String file;
 
   public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    }catch (ParameterException ex){
+      jCommander.usage();
+      return;
+    }
+    generator.run();
 
-    List<GroupDate> groups = generateGroups(count);
-    save(groups, file);
   }
 
-  private static void save(List<GroupDate> groups, File file) throws IOException {
+  private void run() throws IOException {
+    List<GroupDate> groups = generateGroups(count);
+    save(groups, new File (file));
+
+  }
+
+  private  void save(List<GroupDate> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());// проверяем, относительный путь файла и оказывается директория проекта, потому что по разному тесты и программы
     // нужно список сохранить в файл
     Writer writer = new FileWriter(file); //нужно файл открыть на запись

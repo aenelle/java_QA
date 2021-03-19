3a46a7cfca29fc7c9ec675d0911bcbe4181fb742
aenelle <aenelle@gmail.com>
2021-03-19 15:49:47 +0300

    Путь к странице      Личный кабинет / ▶︎ Мои курсы / ▶︎ Программирование на Java для тестировщиков, Б22 / ▶︎ Разделы / ▶︎ 6. Работа с файлами / ▶︎ Лекция 6.5. Загрузка тестовых данных из файла  Лекция 6.5. Загрузка тестовых данных из файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,11 +16,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups(){
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new GroupDate().withName("test1").withHeader("header1").withFooter("footer1")});
-    list.add(new Object[]{new GroupDate().withName("test2").withHeader("header2").withFooter("footer2")});
-    list.add(new Object[]{new GroupDate().withName("test3").withHeader("header3").withFooter("footer3")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[]{new GroupDate().withName(split[0]).withHeader(split[1]).withFooter(split[2])}); // lesson 6.5 time 5.12
+      line = reader.readLine();
+    }
     return list.iterator();
 
   }
@@ -33,6 +38,9 @@ public class GroupCreationTests extends TestBase {
     Groups after = app.group().all();
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
   }
+
+  //если тест ignored @Alexei: сравните разделитель, который используется в файле, с тем, который используется для разбиения прочитанных строк на кусочки
+  // перепроверить правильно ли указан путь к файлу, без опечаток
 
   @Test
   public void testBadGroupCreation() {

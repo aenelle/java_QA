eda9af92aaca704e222ab1c245d16d48f4413cd5
aenelle <aenelle@gmail.com>
2021-03-21 00:46:11 +0300

    Лекция 6.6. Формат XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
 import java.io.File;
@@ -17,6 +18,9 @@ public class GroupDataGenerator {
   public int count;
 @Parameter(names = "-f", description = "Target file")
 public String file;
+@Parameter(names = "-d", description = "Data format")
+public String format;
+
 
   public static void main(String[] args) throws IOException {
     GroupDataGenerator generator = new GroupDataGenerator();
@@ -33,11 +37,28 @@ public String file;
 
   private void run() throws IOException {
     List<GroupDate> groups = generateGroups(count);
-    save(groups, new File (file));
-
+    if (format.equals("csv")) {
+      saveAsCsv(groups, new File(file));
+    }
+    else if(format.equals("xml")) {
+      saveAsXml(groups, new File(file));
+    }
+    else {
+      System.out.println("Unrecognized format" + format);
+    }
   }
 
-  private  void save(List<GroupDate> groups, File file) throws IOException {
+  private void saveAsXml(List<GroupDate> groups, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupDate.class);
+    String xml = xstream.toXML(groups); // в качестве параметра передаем тот объект, который нужно серилизовать, превратить из объектного представления в строчку формата xml
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+
+  private  void saveAsCsv(List<GroupDate> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());// проверяем, относительный путь файла и оказывается директория проекта, потому что по разному тесты и программы
     // нужно список сохранить в файл
     Writer writer = new FileWriter(file); //нужно файл открыть на запись
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
@@ -9,6 +10,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,16 +19,25 @@ public class GroupCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
     String line = reader.readLine();
-    while (line != null){
-      String[] split = line.split(";");
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupDate.class);
+    List<GroupDate> groups = (List<GroupDate>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+
+
+      /*String[] split = line.split(";");
       list.add(new Object[]{new GroupDate().withName(split[0]).withHeader(split[1]).withFooter(split[2])}); // lesson 6.5 time 5.12
       line = reader.readLine();
     }
     return list.iterator();
-
+*/
   }
 
   @Test(dataProvider = "validGroups")

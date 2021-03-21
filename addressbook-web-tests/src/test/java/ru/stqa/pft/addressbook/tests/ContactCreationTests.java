05253670b28bec4e55e7187fcdb839a236354657
aenelle <aenelle@gmail.com>
2021-03-21 03:27:06 +0300

    Update ContactCreationTests.java
    CleanCode

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,19 +3,27 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
-
 import java.io.File;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class ContactCreationTests extends TestBase {
 
+  @Test //тест как по лекции
+  public void testContactCreation1() {
+    app.goTo().gotoHomePage();
+    app.contact().initContactCreating();
+    File photo = new File("src/test/resources/cat.jpg");
+    app.contact().fillContactForm(new ContactData().withFirstName("Ollo").withLastName("Ups").withCompany("LTD")
+            .withMobilePhone("8(888)000-00-00").withEmail("petrova@gmail.com").withPhoto(photo),true);
+    app.contact().submitContactCreating();
+    app.contact().returnHomePages();
+
+  }
 
   @Test //(enabled = false)
   public void testContactCreation() {
-
     Contacts before = app.contact().all();
     app.contact().initContactCreating();
     File photo = new File("src/test/resources/cat.jpg");
@@ -28,10 +36,6 @@ public class ContactCreationTests extends TestBase {
     assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-
-
-
-
   @Test //(enabled = false)
   public void testBadContactCreation() {
 
@@ -42,30 +46,9 @@ public class ContactCreationTests extends TestBase {
     app.contact().create(contact);
     assertThat(app.contact().count(), equalTo(before.size()));
     Contacts after = app.contact().all();
-
-
     assertThat(after, equalTo(before));
   }
-
-  @Test
-  // код по лекции
-  public void testContactCreation1() {
-
-    app.goTo().gotoHomePage();
-    app.contact().initContactCreating();
-    File photo = new File("src/test/resources/cat.jpg");
-    app.contact().fillContactForm(new ContactData().withFirstName("Ollo").withLastName("Ups").withCompany("LTD")
-            .withMobilePhone("8(888)000-00-00").withEmail("petrova@gmail.com").withPhoto(photo),true);
-    app.contact().submitContactCreating();
-    app.contact().returnHomePages();
-
-  }
 }
-
-
-
-
-
 
   /*@Test //не тест, а метод поиска относительной директории
   public void testCurrentDir(){

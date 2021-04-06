3449b432af60a5944f7638c61589d79f0c333bd9
aenelle <aenelle@gmail.com>
2021-04-06 16:41:00 +0300

    Лекция 8.5. Передача файлов на удалённую машину по протоколу FTP
    Part2

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -1,11 +1,11 @@
 <?php
 $g_hostname = 'localhost';
-$g_db_type = 'mysql';
+$g_db_type = 'mysqli';
 $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
-$g_crypto_master_salt
+//$g_crypto_master_salt
 $g_signup_use_captcha = OFF;
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
-$g_signup_use captcha = OFF;
+//$g_signup_use captcha = OFF;

928c7196cb0b38cdc9f06fa2c8c6c2a7b93cc589
aenelle <aenelle@gmail.com>
2021-04-09 13:30:58 +0300

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -34,6 +38,21 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper();
 
+        if ("".equals(properties.getProperty("selenium.server"))){
+            if (browser.equals(BrowserType.FIREFOX)){
+                wd = new FirefoxDriver();
+            }
+            else if (browser.equals(BrowserType.CHROME)){
+                wd = new ChromeDriver();
+            }
+            else if (browser.equals(BrowserType.SAFARI)){
+                wd = new SafariDriver();
+            }
+        }else{
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+        }
         if (browser.equals(BrowserType.FIREFOX)){
             wd = new FirefoxDriver();
         }
